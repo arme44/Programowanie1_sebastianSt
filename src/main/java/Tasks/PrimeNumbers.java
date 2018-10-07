5b756cbe2ebf025461d12b286d1d35bd77278441
Sebastian Stojanowski <armeerance@gmail.com>
2018-10-07 15:36:32 +0200

    1

--- src/main/java/Tasks/PrimeNumbers.java
+++ src/main/java/Tasks/PrimeNumbers.java
@@ -13,7 +13,7 @@ public class PrimeNumbers {
         System.out.println("podaj nr");
         int n = sc.nextInt();
         long startTime = System.currentTimeMillis();
-//        printPrimes(n);
+        printPrimes(n);
 
         printEratostenesPrimes(n);
 
@@ -26,13 +26,13 @@ public class PrimeNumbers {
 
         Arrays.fill(primes, true);
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (primes[i] == true) {
+            if (primes[i]) {
                 for (int j = i * i; j <= n; j += i) primes[j] = false;
             }
         }
 
         for (int i = 2; i <= n; i++) {
-            if (primes[i] == true) {
+            if (primes[i]) {
                 System.out.println(i);
             }
         }
