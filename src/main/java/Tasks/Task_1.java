1f0c7c138806b15a95e355bb6d8a5c1109e36fa1
Sebastian Stojanowski <armeerance@gmail.com>
2018-10-07 11:30:05 +0200

    1

--- src/main/java/Tasks/Task_1.java
+++ src/main/java/Tasks/Task_1.java
@@ -1,8 +1,14 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task_1 {
 
     public static void main(String[] args) {
+        System.out.println("podaj x");
+        Scanner scanner = new Scanner(System.in);
+
+
         factorial();
     }
 
