9efd342f42905a697961b0a0ee6df6439b6769b5
Sebastian Stojanowski <armeerance@gmail.com>
2018-10-27 11:54:44 +0200

    1

--- src/main/java/tasks/testy/Test.java
+++ src/main/java/tasks/testy/Test.java
@@ -1,4 +1,4 @@
-package tasks;
+package tasks.testy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,16 +13,15 @@ public class Test {
 
         List<Integer> chain = Arrays.asList(1, 2, 3, 8, 5, 3, 8);
 
-        int number = getNumber(chain.size());
+        getNumber(chain, chain.size());
 
     }
 
-    public static int getNumber(int i) {
-        List<Integer> chain = Arrays.asList(1, 2, 3, 8, 5, 3, 8);
-        if (i <= 0)
-            return 1;
-        System.out.println(chain.get(i-1) + ",");
-        return i * getNumber(i - 1);
+    public static String getNumber(List<Integer> chain, int i) {
+        if (i == 0)
+            return "";
+        System.out.println(chain.get(i-1));
+        return getNumber(chain, i-1);
     }
 
 }
---
+++ src/main/java/tasks/testy/Test2.java
@@ -0,0 +1,19 @@
+/*
+package tasks;
+
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        System.out.println("Podaj 11 cyfrowy pesel: ");
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+    }
+}
+*/
